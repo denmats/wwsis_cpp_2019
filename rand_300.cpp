90eb38ccf5e1cd184370b32f3d81d39a4c7637c9
Denys Matsuiev <denys.matsuiev@gmail.com>
2019-03-24 08:44:18 +0200

    split rand_300 by functions

--- rand_300.cpp
+++ rand_300.cpp
@@ -8,27 +8,81 @@ using namespace std;
 int tab [300];
 int start,finish,type_of_sorting;
 
-int main()
+int check_range()
 {
-    srand(time(NULL));
-    cout << "Give the range of numbers to generate random 300 numbers" << endl;
     cout << "Give the start number of random range: " << endl;
     cin >> start;
     cout << "Give the end number of random range: " << endl;
     cin >> finish;
-    cout << "------------" << endl;
 
-    if(start > finish){
+    if(start > finish)
+    {
         int buf = start;
         start = finish;
         finish = buf;
     }
+}
 
+int fillTabWith300RandNum(int a, int b)
+{
     for (int i = 0; i < 300; i++)
     {
         tab[i] = rand()%(finish-start+1)+start;
     }
     cout << ""<<endl;
+}
+
+int ascend_sorting()
+{
+    for (int i = 0; i < 300; i++)
+    {
+        for (int j = i; j < 300; j++)
+        {
+            int tmp = tab [i];
+            if (tab[j] < tmp)
+            {
+                tab[i] = tab[j];
+                tab[j] = tmp;
+            }
+        }
+    }
+}
+
+int descend_sorting()
+{
+    for (int i = 0; i < 300; i++)
+    {
+        for (int j = i; j < 300; j++)
+        {
+            int tmp = tab [i];
+            if (tab[j] > tmp)
+            {
+                tab[i] = tab[j];
+                tab[j] = tmp;
+            }
+        }
+    }
+}
+
+void display_table()
+{
+    for (int i = 0; i < 300; i++)
+    {
+        cout << tab[i] << endl;
+    }
+}
+
+
+int main()
+{
+    srand(time(NULL));
+    cout << "Give the range of numbers to generate random 300 numbers" << endl;
+    cout << "------------" << endl;
+
+    check_range();
+
+    fillTabWith300RandNum(start,finish);
+
 
     while(true)
     {
@@ -42,45 +96,16 @@ int main()
         switch(type_of_sorting)
         {
         case 1:
-            for (int i = 0; i < 300; i++)
-            {
-                for (int j = i; j < 300; j++)
-                {
-                    int tmp = tab [i];
-                    if (tab[j] < tmp)
-                    {
-                        tab[i] = tab[j];
-                        tab[j] = tmp;
-                    }
-                }
-            }
-
-            cout << "Ascend sorting" << endl;
-            for (int i = 0; i < 300; i++)
-            {
-                cout << tab[i] << endl;
-            }
+            //Ascend sorting
+            ascend_sorting();
+            display_table();
             break;
 
         case 2:
-            for (int i = 0; i < 300; i++)
-            {
-                for (int j = i; j < 300; j++)
-                {
-                    int tmp = tab [i];
-                    if (tab[j] > tmp)
-                    {
-                        tab[i] = tab[j];
-                        tab[j] = tmp;
-                    }
-                }
-            }
+            //Descend sorting
+            descend_sorting();
+            display_table();
 
-            cout << "Descend sorting" << endl;
-            for (int i = 0; i < 300; i++)
-            {
-                cout << tab[i] << endl;
-            }
             break;
 
         case 3:
